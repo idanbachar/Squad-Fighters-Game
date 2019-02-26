af3b339bf3aa6d4c0658391be48f28cc7179ecdf
Idan <idan_bachar@hotmail.com>
2019-02-26 15:23:47 +0200

    change game name to SquadFighters

---
+++ SquadFighters/SquadFighters/Client/Client.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Xna.Framework.Content;
+
+namespace SquadFighters
+{
+    public class Client
+    {
+        public string ServerIp;
+        public int ServerPort;
+
+        public TcpClient client;
+        public Thread ReceiveThread;
+        public Thread SendThread;
+
+        private string[] ReceivedDataArray;
+        private ContentManager Content;
+
+        public Client(string serverIp, int serverPort, string playerName)
+        {
+            ServerIp = serverIp;
+            ServerPort = serverPort;
+
+            try{
+                client = new TcpClient(ServerIp, ServerPort);
+
+                /*    Connection Succesfull     */
+                SendMessage(playerName + ",Connected.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void SendData()
+        {
+            while (true)
+            {
+                string data = SquadFighters.Player.ToString();
+                SendMessage(data);
+
+                Thread.Sleep(50);
+            }
+        }
+   
+
+        public void SendMessage(string data)
+        {
+            try{
+                NetworkStream stream = client.GetStream();
+                byte[] bytes = Encoding.ASCII.GetBytes(data);
+                stream.Write(bytes, 0, bytes.Length);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public void AddPlayer(string CurrentConnectedPlayerName)
+        {
+            Player player = new Player(CurrentConnectedPlayerName);
+            player.LoadContent(SquadFighters.ContentManager);
+            SquadFighters.Players.Add(CurrentConnectedPlayerName, player);
+        }
+
+        public void ReceiveData()
+        {
+            while (true)
+            {
+                try
+                {
+                    NetworkStream netStream = client.GetStream();
+                    byte[] bytes = new byte[1024];
+                    netStream.Read(bytes, 0, bytes.Length);
+                    string data = Encoding.ASCII.GetString(bytes);
+                    string ReceivedDataString = data.Substring(0, data.IndexOf("\0"));
+                    ReceivedDataArray = ReceivedDataString.Split(',');
+
+                    if (ReceivedDataString.Contains("Connected"))
+                    {
+                        string CurrentConnectedPlayerName = ReceivedDataString.Split(',')[0];
+
+                        if (CurrentConnectedPlayerName != SquadFighters.Player.Name)
+                        {
+                            AddPlayer(CurrentConnectedPlayerName);
+                        }
+                    }
+                    else
+                    {
+                        //new Random().Next(0, Map.Rectangle.Width - 100), new Random().Next(0, Map.Rectangle.Height - 100)
+
+                        string playerName = ReceivedDataArray[0].Split('=')[1];
+                        float playerX = float.Parse(ReceivedDataArray[1].Split('=')[1]);
+                        float playerY = float.Parse(ReceivedDataArray[2].Split('=')[1]);
+                        float playerRotation = float.Parse(ReceivedDataArray[3].Split('=')[1]);
+                        int playerHealth = int.Parse(ReceivedDataArray[4].Split('=')[1]);
+                        bool playerIsShoot = bool.Parse(ReceivedDataArray[5].Split('=')[1]);
+                        float playerDirectionX = float.Parse(ReceivedDataArray[6].Split('=')[1]);
+                        float playerDirectionY = float.Parse(ReceivedDataArray[7].Split('=')[1]);
+
+                        if (SquadFighters.Players.ContainsKey(playerName))
+                        {
+                            SquadFighters.Players[playerName].Name = playerName;
+                            SquadFighters.Players[playerName].Position.X = playerX;
+                            SquadFighters.Players[playerName].Position.Y = playerY;
+                            SquadFighters.Players[playerName].Rotation = playerRotation;
+                            SquadFighters.Players[playerName].Health = playerHealth;
+                            SquadFighters.Players[playerName].IsShoot = playerIsShoot;
+                            SquadFighters.Players[playerName].Direction.X = playerDirectionX;
+                            SquadFighters.Players[playerName].Direction.Y = playerDirectionY;
+
+                            if (playerIsShoot)
+                            {
+                                SquadFighters.Players[playerName].Shoot();
+                            }
+                        }
+                        else
+                        {
+                            AddPlayer(playerName);
+                        }
+
+                    }
+
+                    Console.WriteLine(ReceivedDataString);
+
+                    Thread.Sleep(50);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        public void CloseConnection()
+        {
+            try{
+                ReceiveThread.Abort();
+                SendThread.Abort();
+                client.Close();
+            }
+            catch (Exception e)
+            {
+
+            }
+            
+        }
+    }
+}
---
+++ SquadFighters/SquadFighters/Map/Items/Shield/Shield.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace SquadFighters
+{
+    public class Shield : Item
+    {
+        public int Armor;
+        public ShieldType ItemType;
+        public ShieldBar[] ShieldBars;
+
+        public Shield(Vector2 itemPosition, ShieldType itemType) : base(itemPosition)
+        {
+            ItemType = itemType;
+            ShieldBars = new ShieldBar[3];
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            Texture = content.Load<Texture2D>("images/items/shields/" + GetShieldName());
+
+            for(int i = 0; i < ShieldBars.Length; i++)
+            {
+                ShieldBars[i] = new ShieldBar(ItemType, new Vector2(i * 75, 50));
+                ShieldBars[i].LoadContent(content);
+            }
+        }
+
+        private string GetShieldName()
+        {
+            switch (ItemType)
+            {
+                case ShieldType.Shield_Level_1:
+                    return "shield_lv1";
+                case ShieldType.Shield_Level_2:
+                    return "shield_lv2";
+                case ShieldType.Shield_Rare:
+                    return "shield_rare";
+                case ShieldType.Shield_Legendery:
+                    return "shield_legendery";
+                default:
+                    return "shield_lv1";
+            }
+        }
+
+        public override void Update()
+        {
+            Rectangle = new Rectangle((int)Position.X, (int)Position.Y, Texture.Width, Texture.Height);
+        }
+
+        public int GetArmor()
+        {
+            return new Random().Next(15, 43);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, Color.White);
+        }
+
+        public override string ToString()
+        {
+            return Armor.ToString();
+        }
+    }
+}
